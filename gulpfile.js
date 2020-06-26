a33bafd3b23e492f17570b25e8347b6f0dcb585a
programmerOt <62360131+programmerOt@users.noreply.github.com>
2020-06-26 16:50:24 +0400

    added gulp in project

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	sourcemaps = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create(),
+	concatCss = require('gulp-concat-css'),
+	sass = require('gulp-sass');
+
+
+function css_style(done) {
+	gulp.src('./task 1.1/styles/*.css')
+		.pipe(concatCss("style.css"))
+		.pipe(gulp.dest('./css/'))
+		.pipe(browserSync.stream())
+	done();
+}
+
+function sync(done) {
+	browserSync.init({
+		server: {
+			baseDir: "./task 1.1/"
+		},
+		port: 3000
+	})
+}
+
+function browserReload(done) {
+	browserSync.reload()
+	done()
+}
+
+function watchFile() {
+	gulp.watch('./task 1.1/styles/*.css', css_style);
+	gulp.watch('./**/*.html', browserReload);
+}
+
+ gulp.task('default', gulp.parallel(sync, watchFile))
