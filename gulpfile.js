0c1cbd0d589ba931219086b5eba2ed782f6559b9
programmerOt <62360131+programmerOt@users.noreply.github.com>
2020-06-29 01:49:42 +0400

    added postcss and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	browserSync = require('browser-sync').create(),
 	concatCss = require('gulp-concat-css'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('gulp-autoprefixer');
 
 
 function css_style(done) {
 	gulp.src('./src/scss/**/*.scss') 
 	.pipe(sourcemaps.init())
 		.pipe(sass())
+		.pipe(autoprefixer({overrideBrowserlist: ['last 2 versions']}))
 		.pipe(gulp.dest('src/build/'))
 		.pipe(browserSync.stream());
 }
